46b8bfdaaae08c768bcbcf7a7c7c633678b141b5
Matheus Iacono <matheus@iacono.com.br>
2018-02-22 13:00:04 -0300

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+
+const extractSass = new ExtractTextPlugin({
+  filename: "bundle.css",
+  disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "main.js"),
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: "css-loader"
+            },
+            {
+              loader: "sass-loader"
+            }
+          ],
+          fallback: "style-loader"
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|ico)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html"
+    }),
+    extractSass
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
+};
