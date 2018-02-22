49a03940f9f500476d063483df6988cb21d9ceea
Matheus Iacono <matheus@iacono.com.br>
2018-02-22 13:06:06 -0300

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    extends: 'airbnb-base',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    plugins: ['react'],
+    rules: {
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+    },
+    settings: {
+      react: { pragma: 'createVNode' },
+    },
+    globals: {
+      document: true,
+      window: true,
+    },
+  };
