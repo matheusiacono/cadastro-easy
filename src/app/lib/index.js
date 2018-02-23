a6a41c01b47c46aa5bdc8eca9dc3d97795803e00
Matheus Iacono <matheus@iacono.com.br>
2018-02-23 14:20:31 -0300

    exporting VirtualNode

--- src/app/lib/index.js
+++ src/app/lib/index.js
@@ -1,4 +1,5 @@
 import App from './core/app';
+import VirtualNode from './core/virtual-node';
 import createVNode from './core/create-v-node';
 
-export { App, createVNode };
+export { App, VirtualNode, createVNode };
