59eb14a2cff061baea0211601c79f5b58a5d8d18
Matheus Iacono <matheus@iacono.com.br>
2018-02-24 01:22:19 -0300

    deleting mask prop from NumberMask component

--- src/app/lib/components/number-mask/index.js
+++ src/app/lib/components/number-mask/index.js
@@ -5,25 +5,26 @@ import NumberInput from '../number-input';
 export default class NumberMask extends VirtualNode {
   render() {
     const nodeProps = { ...this.props };
-    const { oninput } = nodeProps;
+    const { oninput, mask } = nodeProps;
 
     delete nodeProps.oninput;
+    delete nodeProps.mask;
 
     const inputHandler = (e) => {
       const unMaskedValue = e.target.value.replace(/\D+/g, '');
-      e.target.value = applyMask(unMaskedValue, nodeProps.mask);
+      e.target.value = applyMask(unMaskedValue, mask);
 
       if (oninput) {
-        const m =
-          typeof nodeProps.mask === 'function' ? nodeProps.mask(unMaskedValue) : nodeProps.mask;
+        const m = typeof mask === 'function' ? mask(unMaskedValue) : mask;
         const countRawSize = m.match(/X/g).length;
         oninput(unMaskedValue.substring(0, countRawSize));
       }
     };
 
     if (nodeProps.value) {
-      nodeProps.value = applyMask(nodeProps.value.replace(/\D+/g, ''), nodeProps.mask);
+      nodeProps.value = applyMask(nodeProps.value.replace(/\D+/g, ''), mask);
     }
+
     nodeProps.oninput = e => inputHandler(e);
     return <NumberInput {...nodeProps} />;
   }
