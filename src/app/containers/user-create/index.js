c5054a7737c0e0feafc14ce68e41e86bb1344a26
Matheus Iacono <matheus@iacono.com.br>
2018-02-26 04:28:32 -0300

    creating UserCreate container to render the create form page

---
+++ src/app/containers/user-create/index.js
@@ -0,0 +1,23 @@
+import '../form-header.scss';
+import { VirtualNode, createVNode } from '../../lib';
+import { Link } from '../../lib/components/router';
+import UserForm from '../../components/user-form';
+
+export default class UserCreate extends VirtualNode {
+  static render() {
+    const { addUser } = this.getActions();
+    const { submittingForm } = this.getState();
+
+    return (
+      <div>
+        <h1 class="page-title">Novo Usuário</h1>
+        <p>
+          <Link class="go-back" to="/">
+            <i class="icon icon-back" /> Voltar
+          </Link>
+        </p>
+        <UserForm onsubmit={addUser} loading={submittingForm} />
+      </div>
+    );
+  }
+}
